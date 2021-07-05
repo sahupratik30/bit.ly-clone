d2d8a77c167ab482b1f47b6a53d66367f272f4ab
Pratik Sahu <sahupratik30@gmail.com>
2021-07-05 15:49:23 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const createHttpError = require("http-errors");
+const shortid = require("shortid");
+const ShortUrl = require("./models/shortUrl");
 const mongoose = require("mongoose");
 mongoose
   .connect("mongodb://localhost:27017/urlShortener", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    useCreateIndex: true,
   })
   .then(() => {
     console.log("Connection Successful...");
@@ -12,24 +16,56 @@ mongoose
     console.log("Connection failed");
   });
 const app = express();
-const ShortUrl = require("./models/shortUrl");
+app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 const PORT = process.env.port || 5000;
 app.set("view engine", "ejs");
 app.get("/", async (req, res) => {
-  const shortUrls = await ShortUrl.find();
-  res.render("index", { shortUrls: shortUrls });
+  res.render("index");
 });
-app.post("/shortUrl", async (req, res) => {
-  await ShortUrl.create({ full: req.body.url });
-  res.redirect("/");
+app.post("/", async (req, res, next) => {
+  const url = req.body.url;
+  try {
+    if (!url) {
+      throw createHttpError.BadRequest("Enter a valid url");
+    }
+    const urlExists = await ShortUrl.findOne({ full: url });
+    if (urlExists) {
+      res.render("index", {
+        header: `${req.headers.host}`,
+        short_url: `${urlExists.short}`,
+      });
+      return;
+    }
+    const shortUrl = new ShortUrl({ full: url, short: shortid.generate() });
+    const result = await shortUrl.save();
+    res.render("index", {
+      header: `${req.headers.host}`,
+      short_url: `${result.short}`,
+    });
+  } catch (error) {
+    next(error);
+  }
 });
-app.get("/:shortUrl", async (req, res) => {
-  const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl });
-  if (shortUrl == null) return res.sendStatus(404);
-  shortUrl.clicks++;
-  shortUrl.save();
-  res.redirect(shortUrl.full);
+app.get("/:shortId", async (req, res, next) => {
+  try {
+    const shortId = req.params.shortId;
+    const result = await ShortUrl.findOne({ short: shortId });
+    if (!result) {
+      throw createHttpError.NotFound("URL does not exist");
+    }
+    res.redirect(result.full);
+  } catch (error) {
+    next(error);
+  }
+});
+app.use((req, res, next) => {
+  next(createHttpError.NotFound());
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.render("index", { error: err.message });
 });
 
 app.listen(PORT, () => {
